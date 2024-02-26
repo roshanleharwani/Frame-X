bc7ae9872d2f5d85b6b41392d3b3b2ead004139d
Roshan Laharwani <roshanleharwani@gmail.com>
2024-02-26 15:10:19 +0530

    Login mechanism implemented

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, flash, session
-from flask import render_template
 from flask_pymongo import PyMongo
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/Database"
@@ -8,25 +8,32 @@ mongo = PyMongo(app)
 users = mongo.db.users
 app.config["SECRET_KEY"] = "kjsdfjsdjfsdfosfpsoifjsodfosjfosdfosjfsf"
 
+bcrypt = Bcrypt(app)
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    if "user" in session:
+        return render_template("internship.html")
+
     if request.method == "POST":
         name = request.form["name"]
         password = request.form["password"]
-        print(name, password)
         user = users.find_one({"name": name})
         if user:
-            if user.get("password") == password:
+            if bcrypt.check_password_hash(user.get("password"), password):
                 session["user"] = name
-                return redirect("/internship")
+                flash("  Success alert! You have Logged in successfully")
+                return render_template("internship.html")
             else:
-                flash("Wrong Password! Please try again.")
+                flash("  Wrong Password! Please try again.  ")
                 return redirect(request.url)
         else:
-            flash("User Does not exist !!")
+            flash("  User Does not exist !!  ")
             return redirect(request.url)
-    return render_template("index.html")
+
+    else:
+        return render_template("index.html")
 
 
 @app.route("/signup", methods=["GET", "POST"])
@@ -37,15 +44,26 @@ def signup():
         email = request.form.get("email")
         password = request.form.get("password")
         confirm_password = request.form.get("confirm_password")
-        if name and email and phone and password and password == confirm_password:
-            users.insert_one(
-                {"name": name, "phone": phone, "email": email, "password": password}
-            )
-            flash("Successfully signed up")
-            return redirect("/internship")
+        if name and email and phone and password and password and confirm_password:
+            if password == confirm_password:
+                hpassword = bcrypt.generate_password_hash(password).decode("utf-8")
+                users.insert_one(
+                    {
+                        "name": name,
+                        "phone": phone,
+                        "email": email,
+                        "password": hpassword,
+                    }
+                )
+                flash("   Success alert! You have Signed UP successfully ")
+                return render_template("internship.html")
+            else:
+                flash("Password does not match !!")
+                return render_template(request.url)
+
         else:
-            flash("Failed to sign up")
-            return render_template("signup.html")
+            flash("All Fields are required")
+            return render_template(request.url)
     return render_template("signup.html")
 
 
@@ -54,14 +72,16 @@ def contact():
     return render_template("contact.html")
 
 
-@app.route("/internship")
-def internship():
-    return render_template("internship.html")
-
-
 @app.route("/apply")
 def apply():
     return render_template("apply.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    flash("  Info alert! You have Logged Out Successfully  ")
+    return redirect("/")
 
 
 if __name__ == "__main__":
